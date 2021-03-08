5228743b2019d3033fc5ac1772bb45ec1310a008
Qasim Khan <49075348+qaqak@users.noreply.github.com>
2021-03-08 17:11:16 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-python depthai_demo.py -cnn vehicle-license-plate-detection-barrier-0106 -s previewout metaout jpegout
+python3 depthai_demo.py -cnn vehicle-license-plate-detection-barrier-0106 -s previewout metaout jpegout
